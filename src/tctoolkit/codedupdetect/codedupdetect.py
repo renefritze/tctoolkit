4aa6d13dbc92030be2a433e272a2f8009a5e5bff
Rene Milk <rene.milk@uni-muenster.de>
2012-07-31 17:31:54 +0200

    adds in-source comment inserting to CodeDupDetect

--- src/tctoolkit/codedupdetect/codedupdetect.py
+++ src/tctoolkit/codedupdetect/codedupdetect.py
@@ -12,47 +12,76 @@ TC Toolkit is hosted at http://code.google.com/p/tctoolkit/
 import matchstore
 from rabinkarp import RabinKarp
 from tokenizer import Tokenizer
+import tempfile
+import os
+import hashlib
 
 class CodeDupDetect:
-    def __init__(self,filelist, minmatch=100):
-        self.matchstore = matchstore.MatchStore(minmatch)
-        self.minmatch = minmatch #minimum number of tokens to be matched.
-        self.filelist = filelist
-        self.foundcopies = False
+	def __init__(self,filelist, minmatch=100):
+		self.matchstore = matchstore.MatchStore(minmatch)
+		self.minmatch = minmatch #minimum number of tokens to be matched.
+		self.filelist = filelist
+		self.foundcopies = False
 
-    def __findcopies(self):
-        totalfiles = len(self.filelist)
-        i=0
-        for srcfile in self.filelist:
-            i=i+1
-            print "Analyzing file %s (%d of %d)" %(srcfile,i,totalfiles)
-            tknzr = Tokenizer(srcfile)
-            rk = RabinKarp(self.minmatch,self.matchstore)
-            rk.addAllTokens(tknzr)            
-        self.foundcopies = True
-        
-    def findcopies(self):
-        if( self.foundcopies == False):
-            self.__findcopies()
-        return(self.matchstore.iter_matches())
+	def __findcopies(self):
+		totalfiles = len(self.filelist)
+		i=0
+		for srcfile in self.filelist:
+				i=i+1
+				print "Analyzing file %s (%d of %d)" %(srcfile,i,totalfiles)
+				tknzr = Tokenizer(srcfile)
+				rk = RabinKarp(self.minmatch,self.matchstore)
+				rk.addAllTokens(tknzr)
+		self.foundcopies = True
 
-    def printmatches(self,output):
-        exactmatches = self.findcopies()
-        #now sort the matches based on the matched line count (in reverse)
-        exactmatches = sorted(exactmatches,reverse=True,key=lambda x:x.matchedlines)
-        matchcount=0
-        
-        for matches in exactmatches:
-            output.write('%s\n'%('='*50))
-            matchcount=matchcount+1
-            output.write("Match %d:\n"%matchcount)
-            fcount = len(matches)            
-            first = True
-            for match in matches:
-                if( first):
-                    output.write("Found an approx. %d line duplication in %d files.\n" % (match.getLineCount(),fcount))
-                    first = False
-                output.write("Starting at line %d of %s\n" % (match.getStartLine(),match.srcfile()))
-            
-        return(exactmatches)            
+	def findcopies(self):
+		if( self.foundcopies == False):
+				self.__findcopies()
+		return(self.matchstore.iter_matches())
 
+	def printmatches(self,output):
+		exactmatches = self.findcopies()
+		#now sort the matches based on the matched line count (in reverse)
+		exactmatches = sorted(exactmatches,reverse=True,key=lambda x:x.matchedlines)
+		matchcount=0
+
+		for matches in exactmatches:
+				output.write('%s\n'%('='*50))
+				matchcount=matchcount+1
+				output.write("Match %d:\n"%matchcount)
+				fcount = len(matches)
+				first = True
+				for match in matches:
+						if( first):
+								output.write("Found an approx. %d line duplication in %d files.\n" % (match.getLineCount(),fcount))
+								first = False
+						output.write("Starting at line %d of %s\n" % (match.getStartLine(),match.srcfile()))
+
+		return(exactmatches)
+
+	def insert_comments(self):
+		begin_no = 0
+		for matches in self.findcopies():
+			infostring = ' '.join(['%s:%i-%i'%(f.srcfile(),f.getStartLine(),f.getLineCount()) for f in matches])
+			for match in matches:
+				fn = match.srcfile()
+				fn_dup = '%s.dup'%fn
+				with open(fn, 'r') as srcfile:
+					with open(fn_dup, 'wb') as out:
+						for i in range(match.getStartLine()):
+							out.write(srcfile.readline())
+						comment = '//!DUPLICATE BEGIN %i -- %s\n'%(begin_no, infostring)
+						out.write(comment)
+						for i in range(match.getLineCount()):
+							line = srcfile.readline()
+							out.write(line)
+							with open('/tmp/%i.dup'%begin_no, 'a') as dup:
+								dup.write(line)
+						comment = '//!DUPLICATE END %i\n'%begin_no
+						out.write(comment)
+						line = srcfile.readline()
+						while line:
+							out.write(line)
+							line = srcfile.readline()
+				os.rename(fn_dup, fn)
+				begin_no += 1
